23e1ef904a1da2731c37605953426773963f81fd
Ixonal <ixonal@gmail.com>
2015-03-03 09:48:01 -0500

    Update using.js

--- using.js
+++ using.js
@@ -1,6 +1,7 @@
 /*
 UsingJs script loader and dependency tracker
-Copyright 2013-2014 Benjamin McGregor (Ixonal)
+https://github.com/Ixonal/UsingJs
+Copyright 2013-2015 Benjamin McGregor (Ixonal)
 Released under the MIT Licence
 http://opensource.org/licenses/MIT
 */
